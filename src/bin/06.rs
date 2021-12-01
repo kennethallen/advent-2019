bb5891361fc37143568aa648183cf5d1bc883680
Kenneth Allen <kenneth@mostly.me>
2021-12-01 16:07:28 +1100

    Refactor 06 part 1

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,34 +1,57 @@
 use advent_2019::*;
-use std::collections::{HashSet, HashMap};
+use std::collections::HashMap;
 
-type Orbits = HashMap<String, HashSet<String>>;
+struct System {
+  bodies: HashMap<String, Body>,
+}
+impl System {
+  fn new() -> System {
+    System {
+      bodies: HashMap::new(),
+    }
+  }
 
-fn count_orbits(orbits: &Orbits, b: &String, depth: usize) -> usize {
-  depth + match orbits.get(b) {
-    Some(bs) => bs.iter().map(|b1| count_orbits(orbits, b1, depth + 1)).sum(),
-    None => 0,
+  fn add_orbit(&mut self, parent_name: &str, child_name: &str) {
+    if let Some(parent) = self.bodies.get_mut(parent_name) {
+      parent.children.push(child_name.to_owned());
+    } else {
+      self.bodies.insert(parent_name.to_owned(), Body {
+        parents: vec![],
+        children: vec![child_name.to_owned()],
+      });
+    }
+    if let Some(child) = self.bodies.get_mut(child_name) {
+      child.parents.push(parent_name.to_owned());
+    } else {
+      self.bodies.insert(child_name.to_owned(), Body {
+        parents: vec![parent_name.to_owned()],
+        children: vec![],
+      });
+    }
+  }
+
+  fn count_orbits(&self, name: &str) -> usize {
+    self.count_orbits_recurse(name, 0)
+  }
+  fn count_orbits_recurse(&self, name: &str, depth: usize) -> usize {
+    depth + self.bodies[name].children.iter()
+      .map(|c| self.count_orbits_recurse(c, depth + 1))
+      .sum::<usize>()
   }
 }
 
+struct Body {
+  parents: Vec<String>,
+  children: Vec<String>,
+}
+
 fn main() {
-  let data: Vec<(String, String)> = read_lines("input/06.txt").unwrap()
-    .map(|line| line.unwrap())
-    .map(|line| {
-      let mut iter = line.split(")");
-      (iter.next().unwrap().to_owned(), iter.next().unwrap().to_owned())
-    })
-    .collect();
-  let mut orbits: Orbits = HashMap::new();
-  for (b0, b1) in data {
-    if let Some(bs) = orbits.get_mut(&b0) {
-      bs.insert(b1);
-    } else {
-      orbits.insert(b0, HashSet::from([b1]));
-    }
+  let mut system = System::new();
+  for line_res in read_lines("input/06.txt").unwrap() {
+    let line = line_res.unwrap();
+    let mut iter = line.split(")");
+    system.add_orbit(iter.next().unwrap(), iter.next().unwrap());
   }
-  let root = orbits.keys()
-      .find(|b| orbits.values().all(|bs| !bs.contains(b.as_str())))
-      .unwrap();
-  let part1 = count_orbits(&orbits, root, 0);
+  let part1 = system.count_orbits("COM");
   println!("Part 1: {}", part1);
 }
